e32a985f612eb0349152cabecaca2ceba183e98f
Jesuuuu <166551340+Yisus0708@users.noreply.github.com>
2026-04-27 10:46:45 -0500

    Rock Commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+/* const date = new Date()
+document.getElementById("year").innerText = date.getFullYear() */
+
+document.querySelector(".menu-toggle").onclick = function(){
+    document.querySelector(".nav__list").classList.toggle("open")
+}
+
+// Write your Js code here 
